ddcaf648b986ce11ca5b3b6b438518e2347f1c7a
Chase Lennartson <chaselennartson@yahoo.com>
2022-11-30 09:11:06 -0600

    update

--- src/main/java/edu/cs/tcu/chineselearningplatform/dao/QuestionRepository.java
+++ src/main/java/edu/cs/tcu/chineselearningplatform/dao/QuestionRepository.java
@@ -7,7 +7,12 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 public interface QuestionRepository extends MongoRepository<Question, String> {
     @Query("{ '_id': ?0}")
     Question findByObjectId(ObjectId id);
+
+    @Query("{'lesson': ?0}")
+    List<Question> findAllByLesson(String LessonId);
 }
--- src/main/java/edu/cs/tcu/chineselearningplatform/dao/VocabRepository.java
+++ src/main/java/edu/cs/tcu/chineselearningplatform/dao/VocabRepository.java
@@ -6,7 +6,12 @@ import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
 
+import java.util.List;
+
 public interface VocabRepository extends MongoRepository<Vocab, String> {
     @Query("{ '_id': ?0}")
     Vocab findByObjectId(ObjectId id);
+
+    @Query("{'lesson': ?0}")
+    List<Vocab> findAllByLesson(String LessonId);
 }
