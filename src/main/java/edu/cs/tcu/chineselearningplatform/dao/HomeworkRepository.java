5758af98b06de6287f6228dde32e1c0f4df3ad1f
Chase Lennartson <chaselennartson@yahoo.com>
2023-01-11 17:46:32 -0600

    homework crud

---
+++ src/main/java/edu/cs/tcu/chineselearningplatform/dao/HomeworkRepository.java
@@ -0,0 +1,16 @@
+package edu.cs.tcu.chineselearningplatform.dao;
+
+import edu.cs.tcu.chineselearningplatform.entity.Homework;
+import edu.cs.tcu.chineselearningplatform.entity.Question;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+import java.util.List;
+
+public interface HomeworkRepository extends MongoRepository<Homework, String> {
+    @Query("{ '_id': ?0}")
+    Homework findByObjectId(ObjectId id);
+
+    @Query("{'section': ?0}")
+    List<Homework> findAllBySection(String SectionId);
+}
