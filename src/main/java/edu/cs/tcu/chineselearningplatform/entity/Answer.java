aaa2ee5cc21197a46061e730abd9b8455742ed88
quynhtdong <71563993+quynhtdong@users.noreply.github.com>
2023-02-05 15:42:04 -0600

    teacher creates assignment with grades & user submits answer to a question

---
+++ src/main/java/edu/cs/tcu/chineselearningplatform/entity/Answer.java
@@ -0,0 +1,38 @@
+package edu.cs.tcu.chineselearningplatform.entity;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.io.Serializable;
+
+@Document("Answer")
+public class Answer implements Serializable {
+    @Id
+    private String id;
+    private String type;
+    private String key;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+}
