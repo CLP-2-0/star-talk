8c12228a97c7625c8026d50ce5ac95af11d359a0
quynhtdong <71563993+quynhtdong@users.noreply.github.com>
2023-02-19 15:12:11 -0600

    execute google drive cloud for file storage --proceed to use client's gmail account

--- src/main/java/edu/cs/tcu/chineselearningplatform/controller/GradedQuestionController.java
+++ src/main/java/edu/cs/tcu/chineselearningplatform/controller/GradedQuestionController.java
@@ -1,12 +1,14 @@
 package edu.cs.tcu.chineselearningplatform.controller;
 
 import edu.cs.tcu.chineselearningplatform.entity.Answer;
-import edu.cs.tcu.chineselearningplatform.entity.GradedQuestion;
 import edu.cs.tcu.chineselearningplatform.entity.util.Result;
 import edu.cs.tcu.chineselearningplatform.entity.util.StatusCode;
 import edu.cs.tcu.chineselearningplatform.service.GradedQuestionService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
+import java.security.GeneralSecurityException;
 
 @Controller
 @RequestMapping("/graded-question")
@@ -18,16 +20,28 @@ public class GradedQuestionController {
     }
 
     /**
-     * Method to save one answer.
+     * Method to save one text answer.
      * @param id of question to be saved, answer to be saved, username.
      * @return Result object that contains flag, status code, message.
      */
     @PostMapping("/answer/{username}/{id}")
     @ResponseBody
-    public Result save(@RequestBody Answer answer, @PathVariable String username, @PathVariable String id) {
+    public Result saveTextAnswer(@RequestBody Answer answer, @PathVariable String username, @PathVariable String id) throws IOException, GeneralSecurityException {
         gradedQuestionService.saveAnswerToAQuestion(answer, username, id);
         return new Result(true, StatusCode.SUCCESS, "Save answer success");
     }
+
+    /**
+     * Method to save one audio answer.
+     * @param id of question to be saved, answer to be saved, username.
+     * @return Result object that contains flag, status code, message.
+     */
+//    @PostMapping(value = "/audio/{username}/{id}")
+//    public Result saveAudio(@RequestBody byte[] audioData, @PathVariable String username, @PathVariable String id) throws IOException {
+//
+//        gradedQuestionService.saveAudioToAQuestion(audioData, username, id);
+//        return new Result(true, StatusCode.SUCCESS, "Save audio success");
+//    }
 
 
     /**
@@ -39,7 +53,6 @@ public class GradedQuestionController {
     @GetMapping("/answers/{id}")
     @ResponseBody
     public Result findAllAnswers(@PathVariable String id) {
-        ;
         return new Result(true, StatusCode.SUCCESS, "Find answers by id success", gradedQuestionService.getAllAnswerForAQuestion(id));
     }
 }
