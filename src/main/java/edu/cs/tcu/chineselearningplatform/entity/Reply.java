a54c926c16af3c8697ce7e2eb59e7129258d443c
dacinve <dacinve@gmail.com>
2023-03-08 10:43:05 -0600

    reply update

--- src/main/java/edu/cs/tcu/chineselearningplatform/entity/Reply.java
+++ src/main/java/edu/cs/tcu/chineselearningplatform/entity/Reply.java
@@ -1,5 +1,6 @@
 package edu.cs.tcu.chineselearningplatform.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -9,4 +10,65 @@ import java.time.LocalDateTime;
 
 @Document("Reply")
 public class Reply implements Serializable {
+    @Id
+    private String id;
+    private String content;
+
+    @DBRef
+    private User replyCreator;
+
+    @DBRef
+    private TopicAnswer topicAnswer;
+
+    private LocalDateTime createdDate;
+    private LocalDateTime lastUpdateDate;
+
+
+    public String getId() {
+        return id;
     }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public User getReplyCreator() {
+        return replyCreator;
+    }
+
+    public void setReplyCreator(User replyCreator) {
+        this.replyCreator = replyCreator;
+    }
+    @JsonIgnore
+    public TopicAnswer getTopicAnswer() {
+        return topicAnswer;
+    }
+
+    public void setTopicAnswer(TopicAnswer topicAnswer) {
+        this.topicAnswer = topicAnswer;
+    }
+
+    public LocalDateTime getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(LocalDateTime createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public LocalDateTime getLastUpdateDate() {
+        return lastUpdateDate;
+    }
+
+    public void setLastUpdateDate(LocalDateTime lastUpdateDate) {
+        this.lastUpdateDate = lastUpdateDate;
+    }
+}
--- src/main/java/edu/cs/tcu/chineselearningplatform/service/ReplyService.java
+++ src/main/java/edu/cs/tcu/chineselearningplatform/service/ReplyService.java
@@ -1,4 +1,32 @@
 package edu.cs.tcu.chineselearningplatform.service;
 
+import edu.cs.tcu.chineselearningplatform.dao.ReplyRepository;
+import edu.cs.tcu.chineselearningplatform.dao.TopicAnswerRepository;
+import edu.cs.tcu.chineselearningplatform.dao.UserRepository;
+import edu.cs.tcu.chineselearningplatform.entity.Reply;
+import edu.cs.tcu.chineselearningplatform.entity.TopicAnswer;
+import edu.cs.tcu.chineselearningplatform.entity.User;
+import org.springframework.stereotype.Service;
+
+@Service
 public class ReplyService {
+
+    private ReplyRepository replyRepository;
+    private TopicAnswerRepository topicAnswerRepository;
+    private UserRepository userRepository;
+
+    public ReplyService(ReplyRepository replyRepository, TopicAnswerRepository topicAnswerRepository, UserRepository userRepository) {
+        this.replyRepository = replyRepository;
+        this.topicAnswerRepository = topicAnswerRepository;
+        this.userRepository = userRepository;
+    }
+
+    public void createReply(String answerId, Reply reply, String username) {
+        TopicAnswer answer = topicAnswerRepository.findById(answerId).get();
+        User replyCreator = userRepository.findByUsername(username);
+        reply.setReplyCreator(replyCreator);
+        answer.addReply(reply);
+        topicAnswerRepository.save(answer);
+        replyRepository.save(reply);
+    }
 }
